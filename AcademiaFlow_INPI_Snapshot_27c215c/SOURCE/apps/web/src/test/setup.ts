d606eff4dbe2e32a730054cf5aa3c6bfa1e3fd04
lmbernardo7520112 <lmbernardo7520112@users.noreply.github.com>
2026-04-13 10:10:28 -0300

    chore(repo): remove INPI snapshot folder from tracking

--- AcademiaFlow_INPI_Snapshot_27c215c/SOURCE/apps/web/src/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
-
-// cleans up the DOM after each test
-afterEach(() => {
-  cleanup();
-});
